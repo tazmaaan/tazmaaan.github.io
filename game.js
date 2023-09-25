0722f5859237888204b627236f078ad4fb68d552
tazmaaan <145970708+tazmaaan@users.noreply.github.com>
2023-09-25 14:21:12 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,19 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const welcomeScreen = document.getElementById("welcome");
-    const playerNameInput = document.getElementById("player-name-input");
-    const startButton = document.getElementById("start-button");
-
-    startButton.addEventListener("click", function () {
-        const playerName = playerNameInput.value.trim();
-        if (playerName !== "") {
-            startGame(playerName);
-        } else {
-            alert("אנא הזן שם באנגלית לפני שתתחיל");
-        }
-    });
 
     function startGame(playerName) {
         console.log(`שחקן ${playerName} התחיל את המשחק`);
         welcomeScreen.style.display = "none";
     }
+
+    startGame("שחקן חדש"); // השורה הזו תציג את הטקסט מיד בטעינת העמוד
 });
