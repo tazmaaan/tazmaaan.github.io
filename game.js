b4066362b56ef5eb827c1d927d9eef9dcdf2fdab
tazmaaan <145970708+tazmaaan@users.noreply.github.com>
2023-09-25 14:32:11 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -1,10 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const welcomeScreen = document.getElementById("welcome");
+// רשימת המשפטים
+const sentences = [
+    "שלום, איך אתה היום?",
+    "זה הזמן לשחק משחק!",
+    "אני אוהב לתכנת בג'אווהסקריפט.",
+    "למה הכוס ריקה? היית צריך להכניס קפה!",
+    "תודה רבה על העזרה שלך."
+];
 
-    function startGame(playerName) {
-        console.log(`שחקן ${playerName} התחיל את המשחק`);
-        welcomeScreen.style.display = "none";
+function getRandomSentence() {
+    const randomIndex = Math.floor(Math.random() * sentences.length);
+    return sentences[randomIndex];
+}
+
+function startGame() {
+    let player1Score = 0;
+    let player2Score = 0;
+
+    const player1Input = document.getElementById("player1-input");
+    const player2Input = document.getElementById("player2-input");
+    const sentenceDisplay = document.getElementById("sentence-display");
+
+    function updateSentence() {
+        const sentence = getRandomSentence();
+        sentenceDisplay.textContent = sentence;
     }
 
-    startGame("שחקן חדש"); // השורה הזו תציג את הטקסט מיד בטעינת העמוד
-});
+    function handlePlayer1Input(event) {
+        if (event.key === "Enter") {
+            const inputText = event.target.value;
+            const displayedSentence = sentenceDisplay.textContent;
+            if (inputText === displayedSentence) {
+                player1Score++;
+            }
+            event.target.value = "";
+            updateSentence();
+        }
+    }
+
+    function handlePlayer2Input(event) {
+        if (event.key === "Enter") {
+            const inputText = event.target.value;
+            const displayedSentence = sentenceDisplay.textContent;
+            if (inputText === displayedSentence) {
+                player2Score++;
+            }
+            event.target.value = "";
+            updateSentence();
+        }
+    }
+
+    player1Input.addEventListener("keydown", handlePlayer1Input);
+    player2Input.addEventListener("keydown", handlePlayer2Input);
+
+    updateSentence();
+}
+
+startGame();
